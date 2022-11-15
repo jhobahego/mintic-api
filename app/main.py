b63c665f15548857b6c58ca5bb195943a8971052
Jhon Hernandez <jhobahego16@hotmail.com>
2022-11-14 20:16:45 -0500

    obtener documentos y registros de compras

--- app/main.py
+++ app/main.py
@@ -11,7 +11,55 @@ async def root():
     return {"message": "hola misiontic"}
 
 usuarios = ['jhon', 'maria', 'heidy']
-documentos = []
+documentos = [
+    {
+        "documento_id":0,
+        "tipo":"fisico",
+        "autor": "Robert C. Martin",
+        "titulo":"clean code",
+        "descripcion":"Handbook of Agile Software Crashmans",
+        "categoria":"desarrollo de software",
+        "stock":12,
+        "precio":40,
+        "editoria":"great editorial",
+        "paginas":450,
+    },
+    {
+        "documento_id":1,
+        "tipo":"digital",
+        "autor": "Andy Hunt & Davis Thomas",
+        "titulo":"the pragmatic programmer",
+        "descripcion":"From journeyman to master",
+        "categoria":"desarrollo de software",
+        "stock":12,
+        "precio":32,
+        "editoria":"Ward Cummingham",
+        "paginas":350,
+    },
+]
+registros = [
+    {
+        "registro_id":0,
+        "usuario_id":0,
+        "documento_id":1,
+        "tipo_venta":"prestamo",
+        "cantidad":1,
+    },
+    {
+        "registro_id":1,
+        "usuario_id":1,
+        "documento_id":0,
+        "tipo_venta":"compra",
+        "cantidad":1,
+    },
+    {
+        "registro_id":2,
+        "usuario_id":1,
+        "documento_id":1,
+        "tipo_venta":"prestamo",
+        "cantidad":1,
+    },
+]
 
 @app.get("/usuarios")
 async def obtener_usuarios():
@@ -28,4 +76,29 @@ async def obtener_documentos():
 
 @app.get("/documentos/{documento_id}")
 async def obtener_documentos(documento_id: int):
-    return documentos[documento_id]
+    for documento in documentos:
+        dato = documento.get("id")
+        if dato == documento_id:
+            return documentos[documento_id]
+    return None
+
+@app.get("/documentos/titulo=/{titulo}")
+async def obtener_documento_por_titulo(titulo:str):
+    for documento in documentos:
+        dato = documento.get("titulo")
+        if dato == titulo:
+            return documento
+    return None
+
+@app.get("/ventas")
+async def obtener_ventas():
+    return registros
+
+
+@app.get("/registro/{documento_id}")
+async def obtener_registro_por_id(documento_id:int):
+    for registro in registros:
+        dato = registro[documento_id]
+        if dato:
+            return registro[documento_id]
+    return None
