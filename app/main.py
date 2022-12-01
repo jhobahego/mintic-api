d4cfcd5afc5199a6246081d57ed7d39feb5acd6a
Jhon Hernandez <jhobahego16@hotmail.com>
2022-11-30 19:32:33 -0500

    Modelos actualizados con Field y clase config

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, EmailStr
 from typing import Optional, Text
 from uuid import uuid4 as uuid
 from bson import ObjectId
@@ -41,33 +41,134 @@ class PyObjectId(ObjectId):
         field_schema.update(type="string")
 
 class Usuario(BaseModel):
-    usuario_id: Optional[str]
-    nombres: str
-    apellidos: str
-    correo: str
-    contra: str
-    pais: str
-    ciudad: str
+    usuario_id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    nombres: str = Field(...)
+    apellidos: str = Field(...)
+    correo: EmailStr = Field(...)
+    contra: str = Field(...)
+    pais: str = Field(...)
+    ciudad: str = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "nombres": "Jane Doe",
+                "apellidos": "hernandez gomez",
+                "correo": "jdoe@example.com",
+                "contra": "pass123",
+                "pais": "Colombia",
+                "ciudad": "Betulia",
+            }
+        }
+
+class ActualizarUsuario(BaseModel):
+    nombres: Optional[str]
+    apellidos: Optional[str]
+    correo: Optional[EmailStr]
+    contra: Optional[str]
+    pais: Optional[str]
+    ciudad: Optional[str]
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "nombres": "Jane Doe",
+                "apellidos": "hernandez gomez",
+                "correo": "jdoe@example.com",
+                "contra": "pass123",
+                "pais": "Colombia",
+                "ciudad": "Betulia",
+            }
+        }
 
 class Documento(BaseModel):
-    documento_id: Optional[str]
-    tipo: str
-    autor: str
-    titulo: str
-    descripcion: Text
-    categoria: str
-    stock: int
-    precio: int
-    editorial: str
-    idioma: str
-    paginas: int
+    documento_id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    tipo_documento: str = Field(...)
+    autor: str = Field(...)
+    titulo: str = Field(...)
+    descripcion: Text = Field(...)
+    categoria: str = Field(...)
+    stock: int = Field(...)
+    precio: int = Field(...)
+    editorial: str = Field(...)
+    idioma: str = Field(...)
+    paginas: int = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "tipo_documento": "digital",
+                "autor": "Robert C. Martin",
+                "titulo": "clean code",
+                "descripcion": "un libro para aprender codigo",
+                "categoria": "desarrollo de software",
+                "stock": "12",
+                "precio": "40",
+                "editorial": "betulia-editoriales",
+                "idioma": "ingles",
+                "paginas": "125",
+            }
+        }
+
+class ActualizarDocumento(BaseModel):
+    tipo_documento: Optional[str]
+    autor: Optional[str]
+    titulo: Optional[str]
+    descripcion: Optional[Text]
+    categoria: Optional[str]
+    stock: Optional[int]
+    precio: Optional[int]
+    editorial: Optional[str]
+    idioma: Optional[str]
+    paginas: Optional[int]
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "tipo_documento": "digital",
+                "autor": "Robert C. Martin",
+                "titulo": "clean code",
+                "descripcion": "un libro para aprender codigo",
+                "categoria": "desarrollo de software",
+                "stock": "12",
+                "precio": "40",
+                "editorial": "betulia-editoriales",
+                "idioma": "ingles",
+                "paginas": "125",
+            }
+        }
 
 class Registro(BaseModel):
-    registro_id: Optional[str]
-    usuario_id: int
-    nombre_documento: str
-    tipo_de_venta: str
-    cantidad: int
+    registro_id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    id_cliente: int = Field(...)
+    id_documento: int = Field(...)
+    titulo_documento: str = Field(...)
+    tipo_de_adquisicion: str = Field(...)
+    cantidad: int = Field(...)
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "cliente_id": "12352252",
+                "documento_id": "12425266353",
+                "titulo_documento": "hernandez gomez",
+                "tipo_de_adquisicion": "prestamo",
+                "cantidad": "124",
+            }
+        }
 
 usuarios = []
 
