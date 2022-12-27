b5d1dccbe5b94fe65c194cc0ed387462076c19f3
Jhon Hernandez <jhobahego16@hotmail.com>
2022-12-27 17:41:39 -0500

    Archivo refactorizado

---
+++ main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+# from routes.usuarios import usuario
+from routes.documentos import documento
+from routes.registros import registro
+from auth.autenticacion import auth
+
+
+app = FastAPI()
+
+# app.include_router(usuario)
+app.include_router(documento)
+app.include_router(registro)
+app.include_router(auth)
+
+
+# origins = [
+#     "http://localhost:8080",
+# ]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="http://localhost:8080",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
