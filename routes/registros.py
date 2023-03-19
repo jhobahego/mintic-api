51c2dfe31b7dbd5de9575ab8c926a6832de92031
Jhon Hernandez <jhobahego16@hotmail.com>
2023-03-18 20:16:58 -0500

    Refactor a rutas de los registro de ventas

--- routes/registros.py
+++ routes/registros.py
@@ -16,7 +16,7 @@ async def obtener_ventas(token: str = Depends(esquema_oauth)):
 
 
 @registro.get("/ventas/usuario/{usuario_id}", response_description="Usuario obtenido", response_model=Registro)
-async def obtener_ventas_de_usuario(usuario_id: int):
+async def obtener_adquisicion_de_usuario(usuario_id: str, token: str = Depends(esquema_oauth)):
     if (registro := await conn["ventas"].find_one({"id_cliente": usuario_id})) is not None:
         return registro
 
@@ -24,16 +24,16 @@ async def obtener_ventas_de_usuario(usuario_id: int):
         status_code=404, detail=f"cliente {usuario_id} no encontrado")
 
 
-@registro.get("/ventas/documento=/{nombre}", response_description="Registro obtenido", response_model=Registro)
-async def obtener_ventas_de_documento(nombre: str):
+@registro.get("/ventas/documento?/{nombre}", response_description="Registro obtenido", response_model=Registro)
+async def obtener_ventas_de_documento(nombre: str, token: str = Depends(esquema_oauth)):
     if (registro := await conn["ventas"].find_one({"titulo_documento": nombre})) is not None:
         return registro
     raise HTTPException(
         status_code=404, detail=f"documento {nombre} no encontrado")
 
 
-@registro.get("/ventas/tipo=/{tipo_de_venta}", response_description="Registro obtenido", response_model=Registro)
-async def obtener_ventas_por_tipo(tipo_de_venta: str):
+@registro.get("/ventas/tipo?/{tipo_de_venta}", response_description="Registro obtenido", response_model=Registro)
+async def obtener_ventas_por_tipo(tipo_de_venta: str, token: str = Depends(esquema_oauth)):
     if (registro := await conn["ventas"].find_one({"tipo_de_adquisicion": tipo_de_venta})) is not None:
         return registro
     raise HTTPException(
@@ -44,5 +44,5 @@ async def obtener_ventas_por_tipo(tipo_de_venta: str):
 async def guardar_registro(registro: Registro = Body(...), token: str = Depends(esquema_oauth)):
     registro = jsonable_encoder(registro)
     registro_insertado = await conn["ventas"].insert_one(registro)
-    registro_creado = await conn["ventas"].find_one({"registro_id": registro_insertado.inserted_id})
+    registro_creado = await conn["ventas"].find_one({"_id": registro_insertado.inserted_id})
     return JSONResponse(status_code=status.HTTP_201_CREATED, content=registro_creado)
