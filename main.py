8d82298b25a1ce51c835bf63e4ddd239ae252250
Jhon Hernandez <jhobahego16@hotmail.com>
2023-06-02 21:01:42 -0500

    Refactor: codigo innecesario

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app.include_router(auth)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080", "http://localhost"],
+    allow_origins=["http://localhost:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
