ff31fe75e5a571d66580656283d419fbe69a276e
Jhon Hernandez <jhobahego16@hotmail.com>
2023-06-03 21:25:48 -0500

    Fix: production cors

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import os
 
 from routes.usuarios import usuario
 from routes.documentos import documento
@@ -16,9 +17,11 @@ app.include_router(registro)
 app.include_router(imagenes)
 app.include_router(auth)
 
+PRODUCTION_URL = os.environ["PRODUCTION_URL"]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:8080"],
+    allow_origins=["http://127.0.0.1:8080", PRODUCTION_URL],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
